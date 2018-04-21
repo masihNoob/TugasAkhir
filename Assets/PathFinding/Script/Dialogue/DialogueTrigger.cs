753fb8b3de12ee3a3c2e7798282060d59e740a94
obi iberian <neraka.malam@gmail.com>
2018-04-22 02:20:41 +0700

    penambahan sistem dialog

---
+++ Assets/PathFinding/Script/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+	public string[] dialogue;
+	public string nameNpc;
+
+	private void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			DialogueSystem.Instance.AddNewDialogue(dialogue, nameNpc);
+		}
+	}
+}
---
+++ Assets/ujicoba/Script/NavigationLine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(LineRenderer))]
+public class NavigationLine : MonoBehaviour {
+
+	// Use this for initialization
+	public NavMeshAgent agent;
+	private LineRenderer line;
+	void Start () {
+		line = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(agent.hasPath){
+			line.positionCount = agent.path.corners.Length;
+			line.SetPositions(agent.path.corners);
+			line.enabled =true;
+		}else
+		{
+			line.enabled =false;
+		}
+	}
+}
